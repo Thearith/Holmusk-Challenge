b76bab4bb885de8b48e7c85bd8b26dd5ba1b073f
thearith <sothearith.sreang@gmail.com>
2016-04-29 15:35:41 +0800

    implement Database on realm

--- Holmusk/app/src/main/java/holmusk/com/holmuskchallenge/Storage/Databases/DatabaseWrapper.java
+++ Holmusk/app/src/main/java/holmusk/com/holmuskchallenge/Storage/Databases/DatabaseWrapper.java
@@ -2,16 +2,17 @@ package holmusk.com.holmuskchallenge.Storage.Databases;
 
 import android.content.Context;
 import android.util.Log;
+import android.util.TimingLogger;
 
 import java.util.ArrayList;
 
 import holmusk.com.holmuskchallenge.Helpers.DateTime;
 import holmusk.com.holmuskchallenge.Models.Food;
 import holmusk.com.holmuskchallenge.Models.FoodIntake;
-import holmusk.com.holmuskchallenge.Models.Portion;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 import io.realm.RealmResults;
+import io.realm.exceptions.RealmMigrationNeededException;
 
 /**
  * Created by thearith on 23/4/16.
@@ -40,7 +41,17 @@ public class DatabaseWrapper {
 
     private DatabaseWrapper(Context context) {
         RealmConfiguration config = new RealmConfiguration.Builder(context).build();
-        realm = Realm.getInstance(config);
+        try {
+            realm = Realm.getInstance(config);
+        } catch (RealmMigrationNeededException e){
+            try {
+                Realm.deleteRealm(config);
+                //Realm file has been deleted.
+                realm = Realm.getInstance(config);
+            } catch (Exception ex){
+                throw ex;
+            }
+        }
     }
 
     public void destroy() {
@@ -58,15 +69,14 @@ public class DatabaseWrapper {
     }
 
     public FoodIntake getFoodIntake(long date) {
-        RealmResults<FoodIntake> foodIntakes = realm.where(FoodIntake.class).
-                beginsWith("date", String.valueOf(date)).findAll();
+        FoodIntake foodIntake = realm.where(FoodIntake.class).
+                equalTo("date", date).findFirst();
 
-        if(foodIntakes.size() == 1) {
-            return foodIntakes.first();
-        } else {
-            Log.e(TAG, "Food intake query cannot be more than 1");
-            return null;
+        if(foodIntake == null) {
+            Log.e(TAG, "Food intake query is null");
         }
+
+        return foodIntake;
     }
 
     public void addFoodToday(Food food) {
@@ -75,6 +85,9 @@ public class DatabaseWrapper {
     }
 
     public void addFood(long date, Food food) {
+        TimingLogger timings = new TimingLogger(TAG, "adding food method");
+
+        setFood(food);
         FoodIntake foodIntake = getFoodIntake(date);
 
         if(foodIntake == null) {
@@ -93,6 +106,33 @@ public class DatabaseWrapper {
 
             realm.commitTransaction();
         }
+
+        timings.dumpToLog();
+    }
+
+    public void removeFoodToday(Food food) {
+        long today = DateTime.getTodayDateInteger();
+        removeFood(today, food);
+    }
+
+    private void removeFood(long date, Food food) {
+        FoodIntake foodIntake = getFoodIntake(date);
+
+        if(foodIntake == null) {
+            realm.beginTransaction();
+
+            FoodIntake intake = realm.createObject(FoodIntake.class);
+            intake.setDate(date);
+            intake.removeFood(food.getId());
+
+            realm.commitTransaction();
+        } else {
+            realm.beginTransaction();
+
+            foodIntake.removeFood(food.getId());
+
+            realm.commitTransaction();
+        }
     }
 
 
@@ -100,12 +140,27 @@ public class DatabaseWrapper {
     * Public methods - Food
     * */
 
+    public ArrayList<ArrayList<Food>> getWeeklyFoods(long[] dates) {
+        ArrayList<ArrayList<Food>> foodsList = new ArrayList<>();
+        for(long date : dates) {
+            ArrayList<Food> foods = getFoods(date);
+            foodsList.add(foods);
+        }
+
+        return foodsList;
+    }
+
     public Food getFood(String id) {
+        TimingLogger timings = new TimingLogger(TAG, "retrieving food method");
+
         RealmResults<Food> food = realm.where(Food.class).beginsWith("id", id).findAll();
+
+        timings.dumpToLog();
+
         return food.first();
     }
 
-    public ArrayList<Food> getFood() {
+    public ArrayList<Food> getFoods() {
         long today = DateTime.getTodayDateInteger();
         return getFoods(today);
     }
@@ -113,10 +168,14 @@ public class DatabaseWrapper {
     public ArrayList<Food> getFoods(long date) {
         ArrayList<Food> foods = new ArrayList<>();
         FoodIntake foodIntake = getFoodIntake(date);
-        String[] foodIds = foodIntake.getAllFoods();
-        for(String foodId : foodIds) {
-            Food food = getFood(foodId);
-            foods.add(food);
+        if (foodIntake != null) {
+            String[] foodIds = foodIntake.getAllFoods();
+            for (String foodId : foodIds) {
+                if(foodId != null && !foodId.equalsIgnoreCase("")) {
+                    Food food = getFood(foodId);
+                    foods.add(food);
+                }
+            }
         }
 
         return foods;
@@ -134,16 +193,5 @@ public class DatabaseWrapper {
     private boolean isFoodExistInDatabase(String id) {
         RealmResults<Food> food = realm.where(Food.class).beginsWith("id", id).findAll();
         return !food.isEmpty();
-    }
-
-
-    /*
-    * Public methods - Portion
-    * */
-
-    public void setPortion(Portion portion) {
-        realm.beginTransaction();
-        realm.copyToRealmOrUpdate(portion);
-        realm.commitTransaction();
     }
 }
