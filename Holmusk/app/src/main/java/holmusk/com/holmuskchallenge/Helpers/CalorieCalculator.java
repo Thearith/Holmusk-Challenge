3dec922a6dbec7fc9446c60ad5eb5cf5cfdf1793
thearith <sothearith.sreang@gmail.com>
2016-04-29 15:33:38 +0800

    calculate nutrients based on list of food

--- Holmusk/app/src/main/java/holmusk/com/holmuskchallenge/Helpers/CalorieCalculator.java
+++ Holmusk/app/src/main/java/holmusk/com/holmuskchallenge/Helpers/CalorieCalculator.java
@@ -1,5 +1,9 @@
 package holmusk.com.holmuskchallenge.Helpers;
 
+import java.util.ArrayList;
+
+import holmusk.com.holmuskchallenge.Models.Food;
+import holmusk.com.holmuskchallenge.Models.Nutrient;
 import holmusk.com.holmuskchallenge.Models.Settings;
 import holmusk.com.holmuskchallenge.Storage.SharedPreferences.SettingsPreferences;
 
@@ -22,4 +26,129 @@ public class CalorieCalculator {
         }
 
     }
+
+    public static Nutrient getNutrient(ArrayList<Food> foods) {
+        double calories = extractCalories(foods);
+        double protein = extractProtein(foods);
+        double fat = extractFat(foods);
+        double carbs = extractCarbs(foods);
+        double cholesterol = extractCholesterol(foods);
+        double sugar = extractSugar(foods);
+        double fibre = extractFibre(foods);
+        double sodium = extractSodium(foods);
+        double potassium = extractPotassium(foods);
+
+        return new Nutrient(calories, protein, fat, carbs, cholesterol, sugar, fibre, sodium, potassium);
+    }
+
+    public static Nutrient getNutrient(Food food) {
+        double calories = food.getCalories();
+        double protein = food.getProtein();
+        double fat = food.getTotalFats();
+        double carbs = food.getTotalCarbs();
+        double cholesterol = food.getCholesterol();
+        double sugar = food.getSugar();
+        double fibre = food.getDietaryFibre();
+        double sodium = food.getSodium();
+        double potassium = food.getPotassium();
+
+        return new Nutrient(calories, protein, fat, carbs, cholesterol, sugar, fibre, sodium, potassium);
+    }
+
+    public static float[] getWeeklyCalories(ArrayList<ArrayList<Food>> foodsList) {
+        float[] calories = new float[7];
+        for(int i=0; i<foodsList.size(); i++) {
+            ArrayList<Food> foods = foodsList.get(i);
+            calories[i] = (float) extractCalories(foods);
+        }
+
+        return calories;
+    }
+
+
+    /*
+    * Extracting nutrients, private methods
+    * */
+
+    private static double extractCalories(ArrayList<Food> foods) {
+        double calories = 0.0f;
+        for(Food food : foods) {
+            calories += food.getCalories();
+        }
+
+        return calories;
+    }
+
+    private static double extractProtein(ArrayList<Food> foods) {
+        double protein = 0.0f;
+        for(Food food : foods) {
+            protein += food.getProtein();
+        }
+
+        return protein;
+    }
+
+    private static double extractFat(ArrayList<Food> foods) {
+        double fat = 0.0f;
+        for(Food food : foods) {
+            fat += food.getTotalFats();
+        }
+
+        return fat;
+    }
+
+    private static double extractCarbs(ArrayList<Food> foods) {
+        double carbs = 0.0f;
+        for(Food food : foods) {
+            carbs += food.getTotalCarbs();
+        }
+
+        return carbs;
+    }
+
+    private static double extractCholesterol(ArrayList<Food> foods) {
+        double cholesterol = 0.0f;
+        for(Food food : foods) {
+            cholesterol += food.getCholesterol();
+        }
+
+        return cholesterol;
+    }
+
+    private static double extractSugar(ArrayList<Food> foods) {
+        double sugar = 0.0f;
+        for(Food food : foods) {
+            sugar += food.getSugar();
+        }
+
+        return sugar;
+    }
+
+    private static double extractFibre(ArrayList<Food> foods) {
+        double fibre = 0.0f;
+        for(Food food : foods) {
+            fibre += food.getDietaryFibre();
+        }
+
+        return fibre;
+    }
+
+    private static double extractSodium(ArrayList<Food> foods) {
+        double sodium = 0.0f;
+        for(Food food : foods) {
+            sodium += food.getSodium();
+        }
+
+        return sodium;
+    }
+
+    private static double extractPotassium(ArrayList<Food> foods) {
+        double potassium = 0.0f;
+        for(Food food : foods) {
+            potassium += food.getPotassium();
+        }
+
+        return potassium;
+    }
+
 }
